001f1e0cb2fd45d138675b33f384af935018c230
Nico <nrgaleano00@gmail.com>
2024-02-14 06:29:41 -0300

    Cambios de organizacion About/Skills

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,3 @@
-import { skills } from "../About/skills.js";
 import '../About/about.css';
 
 export const About = () => {
@@ -9,22 +8,16 @@ export const About = () => {
                 <h5 className="mx-auto">Hola, me llamo Nicolas Galeano, tengo 23 años y soy Desarrollador Fullstack. <br /> Me encuentro en busqueda de mi primer trabajo IT mientras sigo capacitandome. Actualmente me encuentro cursando Tecnicatura en Programacion en la UNSAM ademas del programa Codo a Codo Python. <br /> Recientemente termine mi capacitancion con la Fundacion Pescar y Valtech, en el que me brindaron una capacitacion Fullstack MERN en EducacionIT y una beca de Google de Analista de Datos en Coursera. <br />
                 Gracias a esto, pude obtener mas conocimientos y mejorar mis habilidades</h5>
             </div>
-            <h2 className="h1-custom">Tecnologias</h2>
-            <div className="d-flex flex-wrap justify-content-center align-items-center gap-3 mx-2 w-about-custom">
-                {skills.map((skill, index)=>(
-                    <Cards name={skill.name} icon={skill.icon} key={index}></Cards>
-                ))}
-            </div>
+            <h2 className="h1-custom">Educación</h2>
         </div>
     )
 }
 
-const Cards = ({name, icon}) => {
+
+const CardEduc = () => {
     return(
-    <div className="card w-card">
-        <div className="card-body hover-custom d-flex justify-content-center align-items-center ">
-            <h5 className="card-title card-font-custom m-0 ">{name}</h5>
+        <div>
+            
         </div>
-      </div>
     )
 }
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,25 @@
+import { skills } from "./skills.js";
+
+export const Skills = () => {
+    return(
+        <div>
+        <h2 className="h1-custom">Tecnologias</h2>
+        <div className="d-flex flex-wrap justify-content-center align-items-center gap-3 mx-2 w-about-custom">
+            {skills.map((skill, index)=>(
+                <Cards name={skill.name} icon={skill.icon} key={index}></Cards>
+            ))}
+        </div>
+        </div>
+    )
+}
+
+
+const Cards = ({name, icon}) => {
+    return(
+    <div className="card w-card">
+        <div className="card-body hover-custom d-flex justify-content-center align-items-center ">
+            <h5 className="card-title card-font-custom m-0 ">{name}</h5>
+        </div>
+      </div>
+    )
+}
