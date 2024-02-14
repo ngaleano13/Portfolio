b38301be9027fcd0f2fbc59a4918f9e72bc94f94
Nico <nrgaleano00@gmail.com>
2024-02-14 06:24:57 -0300

    Cambios en los textos about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,8 +5,9 @@ export const About = () => {
     return(
         <div id="about" className="bg-about d-flex flex-column justify-content-center align-items-center text-light gap-5 py-5">
             <h1 className="h1-custom w-75 text-center">Sobre mi</h1>
-            <div className="mx-2 px-2 text-center w-about-custom">
-                <h5 className="mx-auto">Hola!, Me llamo Nicolas Galeano y soy Desarrollador Fullstack, hace unos meses termine la capacitacion en EducacionIT que me brindo la Fundación Pescar y Valtech. Me encuentro en la búsqueda de mi primer trabajo IT mientras me sigo capacitando en mas tecnologías y especializándome en algunas. </h5>
+            <div className="mx-2 px-2 w-about-custom">
+                <h5 className="mx-auto">Hola, me llamo Nicolas Galeano, tengo 23 años y soy Desarrollador Fullstack. <br /> Me encuentro en busqueda de mi primer trabajo IT mientras sigo capacitandome. Actualmente me encuentro cursando Tecnicatura en Programacion en la UNSAM ademas del programa Codo a Codo Python. <br /> Recientemente termine mi capacitancion con la Fundacion Pescar y Valtech, en el que me brindaron una capacitacion Fullstack MERN en EducacionIT y una beca de Google de Analista de Datos en Coursera. <br />
+                Gracias a esto, pude obtener mas conocimientos y mejorar mis habilidades</h5>
             </div>
             <h2 className="h1-custom">Tecnologias</h2>
             <div className="d-flex flex-wrap justify-content-center align-items-center gap-3 mx-2 w-about-custom">
