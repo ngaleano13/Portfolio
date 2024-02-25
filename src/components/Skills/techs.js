9d8627ef02d02845d211b452d61244d248aef719
Nico <nrgaleano00@gmail.com>
2024-02-25 08:02:05 -0300

    Fix importaciones en archivos js

--- src/components/Skills/techs.js
+++ src/components/Skills/techs.js
@@ -1,7 +1,7 @@
 export const skills = [
-    {name: 'HTML', icon:"../../../public/icon/html.png"},
-    {name: 'CSS', icon:"../../../public/icon/css.png"},
-    {name: 'JavaScript', icon:"/../public/icon/js.png"},
+    {name: 'HTML', icon:"/icon/html.png"},
+    {name: 'CSS', icon:"/icon/css.png"},
+    {name: 'JavaScript', icon:"/icon/js.png"},
     {name: 'React', icon:"/icon/react.png"},
     {name: 'TypeScript', icon:"/icon/ts.png"},
     {name: 'NodeJS', icon:"/icon/node.png"},
@@ -12,18 +12,18 @@ export const skills = [
 ]
 
 export const learnSkills = [
-    {name: 'Python', icon:"../../../public/icon/python.png"},
-    {name: 'MySQL', icon:"../../../public/icon/mysql.png"},
-    {name: 'Django', icon:"../../../public/icon/django.png"},
-    {name: 'Flask', icon:"../../../public/icon/flask.png"},
-    {name: 'Vue.js', icon:"../../../public/icon/vue.png"},
-    {name: 'Spring Boot', icon:"../../../public/icon/spring.png"},
+    {name: 'Python', icon:"/icon/python.png"},
+    {name: 'MySQL', icon:"/icon/mysql.png"},
+    {name: 'Django', icon:"/icon/django.png"},
+    {name: 'Flask', icon:"/icon/flask.png"},
+    {name: 'Vue.js', icon:"/icon/vue.png"},
+    {name: 'Spring Boot', icon:"/icon/spring.png"},
 ]
 
 export const softSkills = [
-    {name: 'Flexibilidad', icon:"../../../public/icon/flex.png"},
-    {name: 'Paciencia', icon:"../../../public/icon/clock.png"},
-    {name: 'Aprendizaje', icon:"../../../public/icon/aprendizaje.png"},
-    {name: 'Compañerismo', icon:"../../../public/icon/teamwork.png"},
+    {name: 'Flexibilidad', icon:"/icon/flex.png"},
+    {name: 'Paciencia', icon:"/icon/clock.png"},
+    {name: 'Aprendizaje', icon:"/aprendizaje.png"},
+    {name: 'Compañerismo', icon:"/icon/teamwork.png"},
 ]
 
